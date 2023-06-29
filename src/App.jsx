bba519987fb72fb7f5c948ab64d350ba60b007a9
matiasortubia <matiortubia@gmail.com>
2023-06-29 02:42:09 -0300

    Add: reset button and message

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,29 @@ import { ResultMessage } from "./components/resultMessage/ResultMessage";
 function App() {
   const [tries, setTries] = useState([]);
   const [isGameOver, setGameOver] = useState(false);
-  const [turnCounter, setTurnCounter] = useState(1);
+  const [turn, setTurn] = useState(1);
   const [isVictory, setVictory] = useState(true);
 
   const handleSubmit = (word) => {
     const result = compareWords(word);
     setTries((prevTries) => [...prevTries, { word: word, result: result }]);
-    setTurnCounter(prevTurn => prevTurn + 1);    
-    console.log("Words sent: " + turnCounter);
+    setTurn(prevTurn => prevTurn + 1);    
+    console.log("Words sent: " + turn);
 
     if (isAnswer(result)) { 
       setGameOver(true);
     }
-    else if (turnCounter >= 6) {
+    else if (turn >= 6) {
       setVictory(false);
       setGameOver(true);
     }
+  };
+
+  const handleReset = () => {
+    setTries([]);
+    setGameOver(false);
+    setTurn(1);
+    if (isVictory == false) setVictory(true);
   };
 
   return (
@@ -38,8 +45,12 @@ function App() {
             );
           })}
         </WordsContainer>
-          { isGameOver ? <ResultMessage bgColor={ isVictory ? "victory" : "lose" }>{ isVictory ? "You win!" : "You lose :("}</ResultMessage> :
-                      <WordInputPanel handleSubmit={handleSubmit} /> }
+        { isGameOver ? <ResultMessage bgColor={ isVictory ? "victory" : "lose" }
+                                      handleReset={ handleReset }>
+          <p>{ isVictory ? "You win!" : "You lose :("}</p>
+          <p className="reset-message">Clic here to play again</p>
+        </ResultMessage> :
+        <WordInputPanel handleSubmit={handleSubmit} /> }
       </div>
     </main>
   );
--- src/components/resultMessage/ResultMessage.jsx
+++ src/components/resultMessage/ResultMessage.jsx
@@ -1,15 +1,19 @@
 import { PropTypes } from "prop-types";
 import styles from "./resultMessage.module.css";
 
-const ResultMessage = ({ children, bgColor }) => {
+const ResultMessage = ({ children, bgColor, handleReset }) => {
   return (
-    <div className={`${styles.messageBox} ${styles[bgColor]}`}>{children}</div>
+    <button className={`${styles.messageBox} ${styles[bgColor]}`}
+            onClick={ handleReset }>
+        {children}
+    </button>
   );
 };
 
 ResultMessage.propTypes = {
   children: PropTypes.string.isRequired,
   bgColor: PropTypes.string.isRequired,
+  handleReset: PropTypes.func.isRequired
 };
 
 export { ResultMessage };
