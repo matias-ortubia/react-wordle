cc2f3057ca2e1dbc5138c6325e90141245e1d687
matiasortubia <matiortubia@gmail.com>
2023-07-07 22:19:33 -0300

    Change URL formatter

--- src/utils/getWord.js
+++ src/utils/getWord.js
@@ -1,5 +1,6 @@
 function getWord(n) {
-  const url = import.meta.env.VITE_API_URL.toString() + n.toString() + "?format=json";
+  // const url = import.meta.env.VITE_API_URL.toString() + n.toString() + "?format=json";
+  const url = `${import.meta.env.VITE_API_URL.toString()}${n.toString()}?format=json`
   // const word = axios.get(url).then(res => res.data.word); // Axios version
   const word = fetch(url).then(res => res.json()).then(el => el.word);
   return word;
