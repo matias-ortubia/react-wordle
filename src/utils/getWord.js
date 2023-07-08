832174ffe7e7a200d29bb44495f34d73a15b9808
matiasortubia <matiortubia@gmail.com>
2023-07-07 23:05:41 -0300

    Add fetch headers

--- src/utils/getWord.js
+++ src/utils/getWord.js
@@ -1,8 +1,14 @@
 function getWord(n) {
-  // const url = import.meta.env.VITE_API_URL.toString() + n.toString() + "?format=json";
   const url = `${import.meta.env.VITE_API_URL.toString()}${n.toString()}?format=json`
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  };
   // const word = axios.get(url).then(res => res.data.word); // Axios version
-  const word = fetch(url).then(res => res.json()).then(el => el.word);
+  const word = fetch(url, {
+    headers: headers,
+    method: 'GET'
+  }).then(res => res.json()).then(el => el.word);
   return word;
 }
 
