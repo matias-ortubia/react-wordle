8fafd2efb192daaaf3cd2ef3ed9e20817d4ae78b
matias-ortubia <matiortubia@gmail.com>
2026-08-17 04:47:39 -0300

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://127.0.0.1:8000',
-    }
-  },
   plugins: [react()],
 })
